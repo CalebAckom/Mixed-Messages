d55c47a5101af6061e62e6e8671d7c9b581d8b94
PRINCE TWUMASI ASAMOAH <prince.t.asamoah@gmail.com>
2022-01-14 14:55:11 +0000

    Refactor random generator function

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const { type } = require("os");
+const note = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+const chord = ['maj9', 'maj9#11', 'm11', 'm13', '13', '13b9', '9#11', '7#9b13', '7sus4b9', 'dim7', 'm7b5'];
 
-const note = ['C', 'C#/Db', 'D', 'Eb', 'E', 'F', 'F#/Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
-const chord = ['maj9', 'naj9#11', 'm11', 'm13', '13', '13b9', '9#11', '7#9b13', '7sus4b9', 'dim7', 'm7b5'];
-
-const mixed = (type) =>
-{
-    let num = Math.floor(Math.random() * type.length);
-    return type[num]
+const randomSelect= (array) => {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
 };
 
-console.log('Use the generated melody note or pitch ' + mixed(note) + ' with the chord ' + mixed(note) + mixed(chord));
+let melodyNote1 = randomSelect(note);
+
+console.log(`Start melody on note: ${melodyNote1}`);
