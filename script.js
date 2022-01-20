5516bb39b1b97a20910390eddd7312477e9c39cd
PRINCE TWUMASI ASAMOAH <prince.t.asamoah@gmail.com>
2022-01-20 15:45:07 +0000

    Add note value generation feature to message

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const note = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 const chord = ['maj9', 'maj9#11', 'm11', 'm13', '13', '13b9', '9#11', '7#9b13', '7sus4b9', 'dim7', 'm7b5'];
+const noteValue = ['Whole', 'Half', 'Quarter', 'Sixth', 'Eighth', 'Sixteenth'];
 
 const randomSelect= (array) => {
     let randomIndex = Math.floor(Math.random() * array.length);
@@ -18,11 +19,21 @@ const generateTempo = () => {
     let tempo = Math.abs(Math.floor(Math.random() * (180 - 60 + 1) + 60));
     return tempo;
 }
+
+const generateNoteValue = () => {
+    let randomNoteValue = [];
+    for (let i = 0; i < 4; i++) {
+        randomNoteValue.push(randomSelect(noteValue));
+    }
+    return randomNoteValue.join(' -> ');
+}
 let melodyNote1 = randomSelect(note);
 let chordProg1 = genChordProgression();
 let tempo1 = generateTempo();
+let noteValue1 = generateNoteValue(noteValue);
 
 console.log('Melody and Chord Composition');
 console.log('============================');
-console.log(`Using tempo :${tempo1} BPM`);
+console.log(`Using tempo: ${tempo1} BPM`);
 console.log(`Starting melody on note: ${melodyNote1} \nWith chord progression: ${chordProg1}`);
+console.log(`With note values      : ${noteValue1}`);
